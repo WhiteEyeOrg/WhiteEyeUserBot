2f69bbceb4c0a890a05179135986d42d687b7e0b
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-03-05 18:01:17 +0530

    Add files via upload

---
+++ resources/string_gen.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# (c) https://t.me/TelethonChat/37677
+# This Source Code Form is subject to the terms of the GNU
+# General Public License, v.3.0. If a copy of the GPL was not distributed with this
+# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
+
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
+ok = """██╗    ██╗██╗  ██╗██╗████████╗███████╗███████╗██╗   ██╗███████╗    
+██║    ██║██║  ██║██║╚══██╔══╝██╔════╝██╔════╝╚██╗ ██╔╝██╔════╝    
+██║ █╗ ██║███████║██║   ██║   █████╗  █████╗   ╚████╔╝ █████╗      
+██║███╗██║██╔══██║██║   ██║   ██╔══╝  ██╔══╝    ╚██╔╝  ██╔══╝      
+╚███╔███╔╝██║  ██║██║   ██║   ███████╗███████╗   ██║   ███████╗    
+ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝   ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚══════╝
+"""
+print(ok)
+APP_ID = int(input("Enter APP ID here: \n"))
+API_HASH = input("Enter API HASH here: \n")
+
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    try:
+        session = client.session.save()
+        client.send_message("me", f"String Session \nTap To Copy. \n`{session}`")
+        print("String Generated Sucessfully Check Your Saved Message.")
+    except Exception as sed:
+        print(f"Something Went Wrong While Generating String \nError : {sed}")
