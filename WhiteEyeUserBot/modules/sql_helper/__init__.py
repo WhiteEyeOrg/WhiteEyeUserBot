a212d1020fcd1c3e15ab9c784cd83d1e0d1bf231
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-07-04 13:17:47 +0530

    Update __init__.py

--- WhiteEyeUserBot/modules/sql_helper/__init__.py
+++ WhiteEyeUserBot/modules/sql_helper/__init__.py
@@ -6,6 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from WhiteEyeUserBot.Configs import Config
-from WhiteEyeUserBot.db_start import BASE, SESSION
+from WhiteEyeUserBot.WhiteEyeUserBot.db_start import BASE, SESSION
 
 # -------------------------------------- #
